ab5250523484c242d477404fd1f983ecc2a68a07
dravog7 <dravog77@gmail.com>
2020-03-07 20:06:38 +0530

    base

--- rollup.config.js
+++ rollup.config.js
@@ -1,71 +1,40 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from 'rollup-plugin-commonjs';
+// import purgeCss from '@fullhuman/postcss-purgecss';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import resolve from 'rollup-plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
+import {terser} from 'rollup-plugin-terser';
+import svelte_preprocess_postcss from 'svelte-preprocess-postcss';
 
 const production = !process.env.ROLLUP_WATCH;
-
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/build/bundle.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
-			}
-		}),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
-		!production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+   input: 'src/main.js',
+   output: {
+      format: 'iife',
+      sourcemap: true,
+      name: 'app',
+      file: 'dist/main.js',
+   },
+   plugins: [
+      svelte({
+         dev: !production,
+         preprocess: {
+            style: svelte_preprocess_postcss(),
+         },
+         css: css => {
+            css.write('dist/components.css');
+         },
+      }),
+      resolve(),
+      commonjs(),
+      postcss({
+         extract: true,
+      }),
+      !production && livereload('dist'),
+      production && terser(),
+   ],
+   // watch: {
+   //    clearScreen: false,
+   // },
 };
-
-function serve() {
-	let started = false;
-
-	return {
-		writeBundle() {
-			if (!started) {
-				started = true;
-
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-					stdio: ['ignore', 'inherit', 'inherit'],
-					shell: true
-				});
-			}
-		}
-	};
-}
